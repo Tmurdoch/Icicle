5182faca3292c61834f63685ece04b0132503dec
Thomas <thomasjmurd@gmail.com>
2024-10-28 21:41:39 -0600

    Formatted IceRoot.cpp, added to readme, fixed build script for cross platform

--- Main/src/IceRoot.cpp
+++ Main/src/IceRoot.cpp
@@ -2,91 +2,90 @@
 #include "VulkanUtils.hpp"
 #include "keyboard_movement_controller.hpp"
 
-
 namespace Icicle {
 
+Root *Root::instancePtr = nullptr;
+// RenderManager* Root::renderManager = nullptr;
 
-    Root* Root::instancePtr = nullptr;
-    //RenderManager* Root::renderManager = nullptr;
+Root::Root() {}
 
-    Root::Root() {}
+void Root::startUp() {
 
-    void Root::startUp() {
+  // window
+  IcicleWindow::getInstance()->startUp(WIDTH, HEIGHT, "Icicle");
+  // device
+  LogicalDevice::getInstance()->startUp();
+  // renderer
+  Renderer::getInstance()->startUp();
 
-        //window
-        IcicleWindow::getInstance()->startUp(WIDTH, HEIGHT, "Icicle");
-        //device
-        LogicalDevice::getInstance()->startUp();
-        //renderer
-        Renderer::getInstance()->startUp();
-        
-        //renderSystem
-        RenderSystem::getInstance()->startUp(Renderer::getInstance()->getSwapChainRenderPass());
-
-    }
-
-    
-    void Root::run() {
-        loadGameObjects();
-        Camera camera{};
-
-        auto viewerObject = GameObject::createGameObject();
-        KeyboardMovementController cameraController{};
-
-        auto currentTime = std::chrono::high_resolution_clock::now();
-        while (!IcicleWindow::getInstance()->shouldClose()) {
-            glfwPollEvents();
-
-            auto newTime = std::chrono::high_resolution_clock::now();
-            float frameTime =
-                std::chrono::duration<float, std::chrono::seconds::period>(newTime - currentTime).count();
-            currentTime = newTime;
-
-            cameraController.moveInPlaneXZ(IcicleWindow::getInstance()->getGLFWwindow(), frameTime, viewerObject);
-            camera.setViewYXZ(viewerObject.transform.translation, viewerObject.transform.rotation);
-
-            float aspect = Renderer::getInstance()->getAspectRatio();
-            
-            camera.setPerspectiveProjection(glm::radians(50.0f), aspect, 0.1f, 15.f);
-
-            if (auto commandBuffer = Renderer::getInstance()->beginFrame()) {
-                Renderer::getInstance()->beginSwapChainRenderPass(commandBuffer);
-
-                RenderSystem::getInstance()->renderGameObjects(commandBuffer, gameObjects, camera);
-
-                Renderer::getInstance()->endSwapChainRenderPass(commandBuffer);
-                Renderer::getInstance()->endFrame();
-            }
-        }
-
-        vkDeviceWaitIdle(logicalDevice->device());
-        cleanUp();
-    }
-
-    void Root::loadGameObjects() {
-#if WIN32
-        std::shared_ptr<Model> model =
-            Model::createModelFromFile(LogicalDevice::getInstance(), "D:\\Projects\\VSIcicle\\Main\\Resources\\Models\\diffuseTest.obj");
-#else
-        std::shared_ptr<Model> model =
-            Model::createModelFromFile(LogicalDevice::getInstance(), "../Resources/Models/diffuseTest.obj");
-#endif
-        auto gameObj = GameObject::createGameObject();
-        gameObj.model = model;
-        gameObj.transform.translation = { 0.0f, 0.0f, 2.5f };
-        gameObj.transform.scale = glm::vec3(3.0f);
-        gameObjects.push_back(std::move(gameObj));
-    }
-
-    void Root::cleanUp() {
-        renderSystem->cleanUp();
-        renderer->cleanUp();
-        logicalDevice->cleanUp();
-        window->cleanUp();
-    }
-
-    
-
+  // renderSystem
+  RenderSystem::getInstance()->startUp(
+      Renderer::getInstance()->getSwapChainRenderPass());
 }
 
+void Root::run() {
+  loadGameObjects();
+  Camera camera{};
 
+  auto viewerObject = GameObject::createGameObject();
+  KeyboardMovementController cameraController{};
+
+  auto currentTime = std::chrono::high_resolution_clock::now();
+  while (!IcicleWindow::getInstance()->shouldClose()) {
+    glfwPollEvents();
+
+    auto newTime = std::chrono::high_resolution_clock::now();
+    float frameTime =
+        std::chrono::duration<float, std::chrono::seconds::period>(newTime -
+                                                                   currentTime)
+            .count();
+    currentTime = newTime;
+
+    cameraController.moveInPlaneXZ(IcicleWindow::getInstance()->getGLFWwindow(),
+                                   frameTime, viewerObject);
+    camera.setViewYXZ(viewerObject.transform.translation,
+                      viewerObject.transform.rotation);
+
+    float aspect = Renderer::getInstance()->getAspectRatio();
+
+    camera.setPerspectiveProjection(glm::radians(50.0f), aspect, 0.1f, 15.f);
+
+    if (auto commandBuffer = Renderer::getInstance()->beginFrame()) {
+      Renderer::getInstance()->beginSwapChainRenderPass(commandBuffer);
+
+      RenderSystem::getInstance()->renderGameObjects(commandBuffer, gameObjects,
+                                                     camera);
+
+      Renderer::getInstance()->endSwapChainRenderPass(commandBuffer);
+      Renderer::getInstance()->endFrame();
+    }
+  }
+
+  vkDeviceWaitIdle(logicalDevice->device());
+  cleanUp();
+}
+
+void Root::loadGameObjects() {
+#if WIN32
+  std::shared_ptr<Model> model = Model::createModelFromFile(
+      LogicalDevice::getInstance(),
+      "D:\\Projects\\VSIcicle\\Main\\Resources\\Models\\diffuseTest.obj");
+#else
+  std::shared_ptr<Model> model = Model::createModelFromFile(
+      LogicalDevice::getInstance(), "../Resources/Models/diffuseTest.obj");
+#endif
+  auto gameObj = GameObject::createGameObject();
+  gameObj.model = model;
+  gameObj.transform.translation = {0.0f, 0.0f, 2.5f};
+  gameObj.transform.scale = glm::vec3(3.0f);
+  gameObjects.push_back(std::move(gameObj));
+}
+
+void Root::cleanUp() {
+  renderSystem->cleanUp();
+  renderer->cleanUp();
+  logicalDevice->cleanUp();
+  window->cleanUp();
+}
+
+} // namespace Icicle
--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-cmake -S . -B ./build -G "Unix Makefiles"
+
+if [["$OSTYPE" == "linux-gnu"* ]]; then
+	cmake -S . -B ./build -G "Unix Makefiles"
+elif [["$OSTYPE" == "msys" ]]; then
+	cmake -S . -B ./build -G "MSYS Makefiles"
+fi
 
 make -C ./build
 
